ddb1e1d784f502cd38cae404b085ee5082b5478a
Arthur Cosentino <arthurcose@gmail.com>
2024-07-06 20:59:53 -0400

    Update time script

--- scripts/time.sh
+++ scripts/time.sh
@@ -4,5 +4,17 @@
 
 cargo build -p red_moon_cli --release
 
-echo "red_moon " $({ time ./target/release/red_moon_cli $*; } 2>&1 | grep real)
-echo "lua      " $({ time lua $*; } 2>&1 | grep real)
+MEASUREMENTS=10
+
+time_multiple ()
+{
+  # first arg is a name
+  # second arg is the executable
+  # third arg and beyond is passed through
+  for i in $(seq 1 $MEASUREMENTS)
+    do echo "$1" $({ time $2 ${@:3}; } 2>&1 | grep real)
+  done
+}
+
+time_multiple "red_moon " ./target/release/red_moon_cli $@
+time_multiple "lua      " lua $@
