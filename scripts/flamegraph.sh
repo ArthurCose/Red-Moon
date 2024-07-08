0aa7552cdafb9b402179bcc642ef7f382d0fef3d
Arthur Cosentino <arthurcose@gmail.com>
2024-07-07 22:11:06 -0400

    Remove flamegraph scripts

--- scripts/flamegraph.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# call from workspace root
-
-mkdir _flamegraphs
-cargo flamegraph -p red_moon_cli --profile profiling --output="_flamegraphs/$(basename $1).svg" -- $*
-rm perf.data
--- scripts/time_and_flamegraph.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# call from workspace root
-
-bash scripts/flamegraph.sh $*
-bash scripts/time.sh $*
